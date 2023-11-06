d5f75f75f492c3c868c540341f951ea9fb216f4c
Yalin Arie <105169774+yalinjob@users.noreply.github.com>
2023-11-06 13:41:03 +0200

    Update main.py

--- main.py
+++ main.py
@@ -12,27 +12,21 @@ app = Flask(__name__)
 @app.route('/')
 def proxy():
         
-    url = request.args.get('url')
+        url = request.args.get('url')
 
-   parsed_url = parse_url(url)
+        
+        # CVE-2020-7212 - parse_url() -> _encode_invalid_chars()
+        # CVE-2021-33503 - parse_url()
+        host = parse_url(url).host
+        
+        if host not in ALLOWED_HOSTS:
+                return "Not allowed"
 
-   if parsed_url.scheme not in allowed_schemes:
-    return "Invalid URL scheme"
-parsed_url = urlparse(url)
 
-if parsed_url.scheme not in allowed_schemes:
-    return "Invalid URL scheme"
-
-if parsed_url.hostname not in allowed_hosts:
-    return "Not allowed"
-
-try:
-    response = requests.get(url)
-    response.raise_for_status()  # Check for HTTP request success
-except requests.exceptions.RequestException as e:
-    return f"Error: {e}"
-
-soup = BeautifulSoup(response.text, 'html.parser')
+        r = requests.get(url)
+ 
+        soup = BeautifulSoup(r.text, 'html.parser')
+        
         to_change = soup.find_all(text = re.compile('o'))
         
         for element in to_change:
@@ -40,6 +34,11 @@ soup = BeautifulSoup(response.text, 'html.parser')
             element.replace_with(fixed_text)
             
         return str(soup)
+
+    
+
+if __name__ == '__main__':
+        app.run(port=8080)
 
     
 
