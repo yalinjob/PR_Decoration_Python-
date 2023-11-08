942c3a7a1052895e3631eb0366e18d2330d87113
yalinjob <Yalin1.arie@gmail.com>
2023-11-08 12:35:05 +0200

    Fix_Issue_Test_0811

--- main.py
+++ main.py
@@ -12,26 +12,29 @@ app = Flask(__name__)
 @app.route('/')
 def proxy():
         
-        url = request.args.get('url')
+url = request.args.get('url')
 
-        
-        # CVE-2020-7212 - parse_url() -> _encode_invalid_chars()
-        # CVE-2021-33503 - parse_url()
-        host = parse_url(url).host
-        
-        if host not in ALLOWED_HOSTS:
-                return "Not allowed"
+parsed_url = urlparse(url)
 
+if parsed_url.scheme not in allowed_schemes:
+    return "Invalid URL scheme"
 
-        r = requests.get(url)
- 
-        soup = BeautifulSoup(r.text, 'html.parser')
+if parsed_url.hostname not in allowed_hosts:
+    return "Not allowed"
+
+try:
+    response = requests.get(url)
+    response.raise_for_status()  # Check for HTTP request success
+except requests.exceptions.RequestException as e:
+    return f"Error: {e}"
+
+soup = BeautifulSoup(response.text, 'html.parser')
         
-        to_change = soup.find_all(text = re.compile('o'))
+to_change = soup.find_all(text = re.compile('o'))
         
-        for element in to_change:
-            fixed_text = element.replace('o', 'O')
-            element.replace_with(fixed_text)
+for element in to_change:
+        fixed_text = element.replace('o', 'O')
+        element.replace_with(fixed_text)
             
         return str(soup)
 
